42dba54accdf7058b23566c043c6ffa791bba59d
kurtfalde <engineerfalde@yahoo.com>
2017-07-03 21:20:20 -0700

    Updates

--- get-alaudit.ps1
+++ get-alaudit.ps1
@@ -11,6 +11,21 @@ RPC connectivity to remote systems to connect to via get-winevent to gather back
 
 #>
 
+#Check if NuGet is installed on system and if not then install it used to get PoshRSJob module
+If((Get-PackageProvider -Name NuGet) -eq $null){
+  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser  
+}
+
+#Check if PoshRSJob module is installed and if not to install it
+If((Get-Module -ListAvailable -name PoshRSJob) -eq $null){
+  Install-Module -Name PoshRSJob -Scope CurrentUser -Force  
+}
+Install-Module -Name PoshRSJob -Scope CurrentUser -Force
+
+#Set working directory
+$WorkingDirectory = (Get-Item -Path ".\" -Verbose).FullName
+
+#Declaring variables
 $computers = @()
 
 # Get computer list from text file in local working directory
@@ -20,7 +35,18 @@ $computers += Get-Content -Path ./computers.txt
 $groups = @()
 $groups = "Applocker-Audit, AppLocker-Enforce"
 Foreach($group in $groups) {
-    $computers += get-adgroupmember $group
+    $computers += Get-ADGroupMember -Identity $group
 }
 
 
+  $ALAuditData = $WorkingDirectory + "\ALAuditData.csv"
+  If(Test-Path -Path $ALAuditData){
+    $ALAuditDataTimeString = (Get-ChildItem -Path $ALAuditData).creationtime.tostring("dd.MM.yyyy.HH.mm")
+    $ALAuditDataRenamed = $($WorkingDirectory) + "\ALAuditData" + $($ALAuditDataTimeString) + ".csv"
+    Rename-Item -Path $ALAuditData -newname $ALAuditDataRenamed
+  }
+  #$alauditmtx = New-Object System.Threading.Mutex($false, "ALAuditMutex")
+  #$ALAuditScriptBlock = get-command $($WorkingDirectory)\ALAuditScriptBlock.ps1 | Select-Object -ExpandProperty ScriptBlock
+  #$computers | Start-RSJob -ScriptBlock $DCsScriptBlock -name {$_} -Throttle $RunspaceThreads -ArgumentList $alauditmtx, $ALAuditData
+
+#>
