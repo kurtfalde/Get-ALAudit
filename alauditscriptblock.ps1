e56f71ba6a115608579f2dff3ce852025e4cf3bf
kurtfalde <engineerfalde@yahoo.com>
2017-07-06 17:04:12 -0400

    Update alauditscriptblock.ps1
    Adding getting script/msi logs

--- alauditscriptblock.ps1
+++ alauditscriptblock.ps1
@@ -31,8 +31,6 @@
             $userprincipalname = (Get-ADUser $exedlleventxml.Event.UserData.RuleAndFileData.TargetUser).UserPrincipalName
             $username = (Get-ADUser $exedlleventxml.Event.UserData.RuleAndFileData.TargetUser).Name
 
-            $Computercsv | Add-Member noteproperty ComputerOU $ComputerOU
-
             Clear-Item $aleventcsvdata
        
             $aleventcsvdata = New-Object PSObject -Property @{            
@@ -59,6 +57,47 @@
         }
 
 
+        $scriptmsievents = Get-WinEvent -LogName 'Microsoft-Windows-AppLocker/MSI and Script' -FilterXPath "*[System[(EventID=8006 or EventID=8007)]]"
+
+        foreach($scriptmsievent in $scriptmsievents){
+            
+            $scriptmsieventxml = [xml]$scriptmsievent.ToXml()
+        
+            #Test for constrained language test scripts if so then continue to next event
+            # Match partial path in temp directory with form XXXXXXXX.XXX.PS* or __PSScriptPolicyTest_XXXXXXXX.XXX.PS*
+	        $pattern1 = "\\APPDATA\\LOCAL\\TEMP\\[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
+	        $pattern2 = "\\APPDATA\\LOCAL\\TEMP\\(__PSScriptPolicyTest_)?[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
+	
+            If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern1){continue}
+            If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern2){continue}
+            
+            $userprincipalname = (Get-ADUser $scriptmsieventxml.Event.UserData.RuleAndFileData.TargetUser).UserPrincipalName
+            $username = (Get-ADUser $scriptmsieventxml.Event.UserData.RuleAndFileData.TargetUser).Name
+
+            Clear-Item $aleventcsvdata
+       
+            $aleventcsvdata = New-Object PSObject -Property @{            
+                MachineName         = $computer.name                 
+                MachineOU           = $ComputerOU             
+                MachineOS           = $computer.OperatingSystem
+                UserPrincipalName   = $userprincipalname            
+                UserName            = $username
+                PolicyName          = $scriptmsieventxml.Event.UserData.RuleAndFileData.PolicyName           
+                CreateDate          = $scriptmsieventxml.Event.System.TimeCreated.SystemTime           
+                EventID             = $scriptmsieventxml.Event.System.EventID           
+                FilePath            = $scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath         
+                FileHash            = $scriptmsieventxml.Event.UserData.RuleAndFileData.FileHash           
+                Fqbn                = $scriptmsieventxml.Event.UserData.RuleAndFileData.Fqbn           
+         
+                }
+
+        
+            $alauditmtx.WaitOne(300000)
+            $aleventcsvdata | Export-Csv $ALAuditDataFile -Encoding ASCII -NoTypeInformation -Append
+            $alauditmtx.ReleaseMutex()
+
+
+        }
 
 
 
