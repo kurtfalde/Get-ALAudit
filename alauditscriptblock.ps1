999ac338b34965df877d0e0d265206623adeba09
kurtfalde <engineerfalde@yahoo.com>
2017-07-06 22:17:47 -0400

    Update alauditscriptblock.ps1

--- alauditscriptblock.ps1
+++ alauditscriptblock.ps1
@@ -17,7 +17,7 @@
 
         $aleventcsvdata = New-Object psobject
 
-        $computer = get-adcomputer -Identity $computer.name -Properties OperatingSystem 
+        $computer = get-adcomputer -Identity $computer -Properties OperatingSystem 
         $ComputerOU = $computer.DistinguishedName -creplace "^[^,]*,",""       
 
         #Get events from Microsoft-Windows-AppLocker/EXE and DLL log
@@ -57,7 +57,7 @@
         }
 
 
-        $scriptmsievents = Get-WinEvent -ComputerName $computer -LogName 'Microsoft-Windows-AppLocker/MSI and Script' -FilterXPath "*[System[(EventID=8006 or EventID=8007)]]"
+        $scriptmsievents = Get-WinEvent -ComputerName $computer.name -LogName 'Microsoft-Windows-AppLocker/MSI and Script' -FilterXPath "*[System[(EventID=8006 or EventID=8007)]]"
 
         foreach($scriptmsievent in $scriptmsievents){
             
@@ -66,11 +66,15 @@
             #Test for constrained language test scripts if so then continue to next event
             # Match partial path in temp directory with form XXXXXXXX.XXX.PS* or __PSScriptPolicyTest_XXXXXXXX.XXX.PS*
 	        $pattern1 = "\\APPDATA\\LOCAL\\TEMP\\[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
-	        $pattern2 = "\\APPDATA\\LOCAL\\TEMP\\(__PSScriptPolicyTest_)?[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
-	
+            $pattern2 = "\\APPDATA\\LOCAL\\TEMP\\[0-9]{1}\\[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
+	        $pattern3 = "\\APPDATA\\LOCAL\\TEMP\\(__PSScriptPolicyTest_)?[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
+            $pattern4 = "\\APPDATA\\LOCAL\\TEMP\\[0-9]{1}\\(__PSScriptPolicyTest_)?[A-Z0-9]{8}\.[A-Z0-9]{3}\.PS"
+
             If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern1){continue}
             If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern2){continue}
-            
+            If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern3){continue}
+            If($scriptmsieventxml.Event.UserData.RuleAndFileData.FilePath -match $pattern4){continue}
+
             $userprincipalname = (Get-ADUser $scriptmsieventxml.Event.UserData.RuleAndFileData.TargetUser).UserPrincipalName
             $username = (Get-ADUser $scriptmsieventxml.Event.UserData.RuleAndFileData.TargetUser).Name
 
@@ -98,12 +102,3 @@
 
 
         }
-
-
-
-        
-
-
-        
-
-
